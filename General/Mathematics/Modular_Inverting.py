aeacb1faa9a8d1b4b8a7a7a34f77e8c9a64e0b39
Romain PIERRE <romain.pierre@enseirb-matmeca.fr>
2023-12-20 01:11:04 +0100

    explanation

--- General/Mathematics/Modular_Inverting.py
+++ General/Mathematics/Modular_Inverting.py
@@ -1,6 +1,6 @@
-# (3 * d) % 13 = 1
-# (3 ** 12) % 13 = 1 (Fermat's little theoreme)
-# d = (3 ** 11) % 13
+# 3 * d = 1 [13]
+# 3 ** 12 = 1 [13] (Fermat's little theoreme)
+# d = 3 ** 11 [13]
 
 element = 3
 modulo = 13
